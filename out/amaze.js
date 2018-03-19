e6789a53cd0f41254c06b75a478c4530310869c4
Yiwei Chiao <ywchiao@gmail.com>
2018-03-19 22:00:26 +0800

    .

---
+++ out/amaze.js
@@ -0,0 +1,23 @@
+(function () {
+	'use strict';
+
+	/**
+	 *  @file       index.js
+	 *  @brief      The entry point of Amaze.
+	 *  @author     Yiwei Chiao (ywchiao@gmail.com)
+	 *  @date       03/19/2018 created.
+	 *  @date       03/19/2018 last modified.
+	 *  @version    0.1.0
+	 *  @since      0.1.0
+	 *  @copyright  MIT, © 2018 Yiwei Chiao
+	 *  @details
+	 *
+	 *  The entry point of Amaze.
+	 */
+
+	window.addEventListener('load', function () {});
+
+	// index.js
+
+}());
+//# sourceMappingURL=amaze.js.map
